9470a94321504c99d0b9f32789aee3a439e90b9d
gswcott <dandanwu89@gmail.com>
2021-03-08 16:38:55 +0100

    prototype

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "materialize-css";
+
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+});*/
+
+M.Sidenav.init(document.querySelectorAll(".sidenav")); 
+M.Sidenav.init(document.querySelectorAll("select")); 
